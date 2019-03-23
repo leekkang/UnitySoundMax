49e61c1b1128f6a242d1bd4bad85c84540c326ad
LeeKanghyuk <qkaxhfdl93@gmail.com>
2019-03-23 19:29:33 +0900

    메인 씬, 싱글톤 스크립트 추가

---
+++ Assets/Script/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T instance;
+
+    public static T inst {
+        get {
+            if (instance == null) {
+                GameObject obj;
+                obj = GameObject.Find(typeof(T).Name);
+                if (obj == null) {
+                    obj = new GameObject(typeof(T).Name);
+                    instance = obj.AddComponent<T>();
+                } else {
+                    instance = obj.GetComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+    
+    public void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+}
