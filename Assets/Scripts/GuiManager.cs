ec1fec5b5cbd0abe90537463d01b748dc73d851e
HwangSooHyeon <mikaelh1651@gmail.com>
2019-03-25 00:02:22 +0900

    panel_start 커서이동 구현, panel_select 스크롤뷰 구현

--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -2,12 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuiManager : Singleton<GuiManager> {
-    void Start() {
+public class GuiManager : Singleton<GuiManager> { //manager를 singleton으로 설정
+    
+    public enum CurPanel { //현재 패널 상태를 확인
+            Main,
+            Select,
+            Option,
+            Ingame,
+            Result
+    }
 
+    private GameObject mPanelMain; //패널들을 확인하기 위한 변수선언
+    private GameObject mPanelSelect;
+    private GameObject mPanelOption;
+    private GameObject mPanelIngame;
+    private GameObject mPanelResult;
+
+    public void Open() {
+        mPanelMain = GameObject.Find("panel_main"); //각 변수에 패널을 할당
+        mPanelMain = GameObject.Find("panel_select");
+        mPanelMain = GameObject.Find("panel_option");
+        mPanelMain = GameObject.Find("panel_ingame");
+        mPanelMain = GameObject.Find("panel_result");
     }
     
-    void Update() {
+    public CurPanel GetCurrentPanel() { //현재 어떤 패널인지 확인 후 해당 패널에 해당하는 값을 리턴
+        if (mPanelMain.activeInHierarchy)
+            return CurPanel.Main;
+        else if (mPanelSelect.activeInHierarchy)
+            return CurPanel.Select;
+        else if (mPanelOption.activeInHierarchy)
+            return CurPanel.Option;
+        else if (mPanelIngame.activeInHierarchy)
+            return CurPanel.Ingame;
+        else if (mPanelResult.activeInHierarchy)
+            return CurPanel.Result;
 
+        return CurPanel.Main;
     }
 }
---
+++ Assets/Scripts/panel_main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_main : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
